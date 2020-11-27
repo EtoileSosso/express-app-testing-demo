21261c810b0ae870febba2435705fcd976645dc2
sonia.rouabhi <sonia.rouabhi@labonneagence.com>
2020-11-27 11:02:53 +0100

    bouton

--- app/route.js
+++ app/route.js
@@ -2,41 +2,58 @@ const formValidator = require('./form_validator');
 const photoModel = require('./photo_model');
 
 function route(app) {
-  app.get('/', (req, res) => {
-    const tags = req.query.tags;
-    const tagmode = req.query.tagmode;
+    app.get('/', (req, res) => {
+        const tags = req.query.tags;
+        const tagmode = req.query.tagmode;
 
-    const ejsLocalVariables = {
-      tagsParameter: tags || '',
-      tagmodeParameter: tagmode || '',
-      photos: [],
-      searchResults: false,
-      invalidParameters: false
-    };
+        const ejsLocalVariables = {
+            tagsParameter: tags || '',
+            tagmodeParameter: tagmode || '',
+            photos: [],
+            searchResults: false,
+            invalidParameters: false
+        };
 
-    // if no input params are passed in then render the view with out querying the api
-    if (!tags && !tagmode) {
-      return res.render('index', ejsLocalVariables);
-    }
+        // if no input params are passed in then render the view with out querying the api
+        if (!tags && !tagmode) {
+            return res.render('index', ejsLocalVariables);
+        }
 
-    // validate query parameters
-    if (!formValidator.hasValidFlickrAPIParams(tags, tagmode)) {
-      ejsLocalVariables.invalidParameters = true;
-      return res.render('index', ejsLocalVariables);
-    }
+        // validate query parameters
+        if (!formValidator.hasValidFlickrAPIParams(tags, tagmode)) {
+            ejsLocalVariables.invalidParameters = true;
+            return res.render('index', ejsLocalVariables);
+        }
 
-    // get photos from flickr public feed api
-    return photoModel
-      .getFlickrPhotos(tags, tagmode)
-      .then(photos => {
-        ejsLocalVariables.photos = photos;
-        ejsLocalVariables.searchResults = true;
-        return res.render('index', ejsLocalVariables);
-      })
-      .catch(error => {
-        return res.status(500).send({ error });
-      });
-  });
+        // get photos from flickr public feed api
+        return photoModel
+            .getFlickrPhotos(tags, tagmode)
+            .then(photos => {
+                ejsLocalVariables.photos = photos;
+                ejsLocalVariables.searchResults = true;
+                return res.render('index', ejsLocalVariables);
+            })
+            .catch(error => {
+                return res.status(500).send({error});
+            });
+    });
+
+    app.get('/zip', (req, res) => {
+        const tags = req.query.tags;
+
+        const ejsLocalVariables = {
+            tagsParameter: tags || '',
+            photos: [],
+            searchResults: false,
+            invalidParameters: false
+        };
+
+        // if no input params are passed in then render the view with out querying the api
+        if (!tags) {
+            return res.render('index', ejsLocalVariables);
+        }
+
+    });
 }
 
 module.exports = route;
