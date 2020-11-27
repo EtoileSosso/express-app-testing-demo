1458a7f396282e59cafbd512109f9eb8ec54ad61
sonia.rouabhi <sonia.rouabhi@labonneagence.com>
2020-11-27 16:17:22 +0100

    database

--- app/worker.js
+++ app/worker.js
@@ -4,6 +4,8 @@ const {PubSub} = require('@google-cloud/pubsub');
 const {Storage} = require('@google-cloud/storage');
 const photoModel = require('./photo_model');
 const env = require('../app/public/json/lejson.json');
+const firebaseConfig = require('./firebaseconfig');
+const firebase = require('firebase-admin');
 
 async function quickstart(
     projectId = env.project_id,
@@ -18,19 +20,27 @@ async function quickstart(
     const timeout = 60;
     const profileBucket = 'dmii2bucket';
 
+    const credentials_admin = firebase.credential.cert(env)
+    firebase.initializeApp({
+        credential: credentials_admin,
+        databaseURL: firebaseConfig.databaseUrl,
+        projectId: 'channelmessaging'
+    });
+
     function listenForMessages() {
         const subscription = pubsub.subscription(subscriptionName);
         const queue = [];
 
         let messageCount = 0;
         const messageHandler = async message => {
+            const data = JSON.parse(message.data);
             console.log(`Received message ${message.id}:`);
             console.log(`\tData: ${message.data}`);
             console.log(`\tAttributes: ${message.attributes}`);
             messageCount += 1;
 
             const storage = new Storage(credentials);
-            const file = await storage.bucket(profileBucket).file('public/users/sonia.zip');
+            const file = await storage.bucket(profileBucket).file(`public/users/${data.tags}.zip`);
             const stream = file.createWriteStream({
                 metadata: {
                     contentType: 'application/zip',
@@ -42,17 +52,15 @@ async function quickstart(
             const zip = new ZipStream();
             zip.pipe(stream);
 
-            photoModel.getFlickrPhotos(message.data.tags, message.data.tagMode).then(photos => {
+            photoModel.getFlickrPhotos(data.tags, data.tagMode).then(photos => {
                 for (let i = 0; i < 10; i++) {
                     queue.push({name: `${photos[i].title}.jpg`, url: photos[i].media.b});
                 }
 
-                console.log(queue.length);
-
                 function addNextFile() {
                     const elem = queue.shift()
                     const stream = request(elem.url)
-                    zip.entry(stream, {name: elem.name}, err => {
+                    zip.entry(stream, {name: elem.name}, async err => {
                         if (err) {
                             throw err;
                         }
@@ -60,8 +68,11 @@ async function quickstart(
                             addNextFile()
                         } else {
                             zip.finalize();
+                            const date = Date.now();
+                            await firebase.database().ref(`/sonia/${date}`).set({file: `public/users/${data.tags}.zip`});
+                            const results = await firebase.database().ref(`/sonia`).once('value');
                         }
-                    })
+                    });
                 }
 
                 addNextFile();
